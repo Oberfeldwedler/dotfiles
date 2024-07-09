d832c91fc43b4d35d6ffb59cb854febe6bc594b7
Oberfeldwedler <samuelbrunner8@gmail.com>
2024-07-09 15:41:08 +0200

    update tmux, bashrcd and disable old stuff

---
+++ .bashrc.d/disabled/30_setup_vagrant.sh
@@ -0,0 +1,3 @@
+export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
+export PATH="$PATH:export PATH=/mnt/c/Program Files/Oracle/VirtualBox/"
+export VAGRANT_WSL_WINDOWS_ACCESS_USER_HOME_PATH="/mnt/c/Users/Sammy/"
