217ac73095a5377beda2b4cc8a2bf74fbc813a6c
Oberfeldwedler <samuelbrunner8@gmail.com>
2025-10-19 21:20:26 +0200

    delete vagrant stuff

--- .bashrc.d/disabled/30_setup_vagrant.sh
+++
@@ -1,3 +0,0 @@
-export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
-export PATH="$PATH:export PATH=/mnt/c/Program Files/Oracle/VirtualBox/"
-export VAGRANT_WSL_WINDOWS_ACCESS_USER_HOME_PATH="/mnt/c/Users/Sammy/"
